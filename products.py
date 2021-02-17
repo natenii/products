596e70803d989144febf7fa411b90e816f94a771
Nate Ni <natengai.nn@gmail.com>
2021-02-17 16:07:01 +0800

    refactor products.py

--- products.py
+++ products.py
@@ -1,29 +1,50 @@
-#讀取檔案
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+import os # operating
 
+#讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding='utf-8') as f:
+                for line in f:
+                    if '商品,價格' in line:
+                        continue
+                    name, price = line.strip().split(',')
+                    products.append([name, price])
+    return products
+    
 #使用者輸入
-while True:
-    name = input('請輸入商品名稱： ')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格： ')
-    price = int(price)
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱： ')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格： ')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
 #印出所有購買紀錄
-for p in products:
-    print(p[0], '價格是', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '價格是', p[1])
 
 #寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-    f.write('商品,價格\n')
-    for p in products:
-        f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('找到檔案！')
+        products = read_file(filename)
+    else:
+        print('找不到檔案！')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
